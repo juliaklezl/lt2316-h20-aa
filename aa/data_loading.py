730e29628fe4cad65945bd0ef5bf45634a2410ea
Julia Klezl <gusklezju@GU.GU.SE@mltgpu.flov.gu.se>
2020-09-23 12:32:59 +0200

    rebuilding data loader

--- aa/data_loading.py
+++ aa/data_loading.py
@@ -65,6 +65,72 @@ class DataLoader(DataLoaderBase):
         super().__init__(data_dir=data_dir, device=device)
 
 
+    def fill_dfs(self, file_list):  # read data from xml into dataframes
+        self.file_list = file_list
+        for file in file_list:
+            if str(file).split("/")[2] == "Test":
+                split = "test"
+            else:
+                split = "train"
+            tree = ET.parse(file)
+            root = tree.getroot()
+            for elem in root:
+                sent_id = elem.get("id")
+                sentence = elem.get("text")
+                tokens = self.get_tokens(sentence)
+                for token in tokens:
+                    token_id = self.get_id(token, self.id2word)
+                    start_char = token[1]
+                    end_char = token[2]
+                    self.data_df.loc[self.x] = [sent_id, token_id, start_char, end_char, split]
+                    self.x += 1
+                for subelem in elem:
+                    if subelem.tag == "entity":
+                        ner_id = self.get_id(subelem.get("type"), self.id2ner)
+                        if len(subelem.get("charOffset").split("-")) == 2:
+                            start_char, end_char = subelem.get("charOffset").split("-")
+                            self.ner_df.loc[self.x] = [sent_id, ner_id, start_char, end_char]
+                            self.x += 1
+                        else:
+                            for word in subelem.get("charOffset").split(";"):
+                                start_char, end_char = word.split("-")
+                                self.ner_df.loc[self.x] = [sent_id, ner_id, start_char, end_char]
+                                self.x += 1
+        return self.data_df, self.ner_df
+                            
+                        
+                
+                
+    def get_id(self, token, dic):  ## !!!!!
+        self.token = token
+        self.dic = dic
+        for key, value in dic.items():  
+            if token == value:
+                return key
+            dic[len(dic)] = token
+            return len(dic)
+        
+    
+    def get_tokens(self, sentence):  # split the sentence into tokens with start and end characters
+        self.sentence = sentence
+        tokens = sentence.strip(".").strip(",").split(" ")  # remove . and , TODO: also remove ()? And what about stopwords, e.g., etc..?
+        tokens_with_numbers = []
+        i = 0
+        for token in tokens:
+            start_char = i
+            end_char = i + len(token)-1
+            i += len(token) + 1
+            tokens_with_numbers.append((token, start_char, end_char))
+        return tokens_with_numbers
+            
+            
+            
+            
+            
+                    
+                
+    
+    
     def _parse_data(self,data_dir):
         # Should parse data in the data_dir, create two dataframes with the format specified in
         # __init__(), and set all the variables so that run.ipynb run as it is.
@@ -72,38 +138,58 @@ class DataLoader(DataLoaderBase):
         # NOTE! I strongly suggest that you create multiple functions for taking care
         # of the parsing needed here. Avoid create a huge block of code here and try instead to 
         # identify the seperate functions needed.
+        
+        # initialize the data structures
         self.data_df = pd.DataFrame(columns = ["sentence_id", "token_id", "char_start_id", "char_end_id", "split"])
-        self.ner_df = pd.DataFrame(columns = ["sentence_id", "ner_id", "char_start_id", "char_end_id"])
-        # look through directories and find all xmls - 1st step, get all filenames 
-        # read/parse xmls  -> next steps: get sent-id, then make token-and ner lists, clean up this function
-        # extract ids
-        # split train into train and val
+        self.ner_df = pd.DataFrame(columns = ["sentence_id", "ner_id", "char_start_id", "char_end_id"]) 
+        self.id2word = {}
+        self.id2ner = {}
+        self.x = 1  # count for dataframe rows
+        
+        # next steps: split train into train and val, make more efficient!!
+        
+        
+        # read in the files
         allfiles = Path(data_dir)
         file_list = [f for f in allfiles.glob('**/*.xml') if f.is_file()]
-        x = 1
-        for file in file_list:
-            if str(file).split("/")[2] == "Test":
-                split = "test"
-            else:
-                split = "train"
-            print("file no", x)
-            print(file)
-            tree = ET.parse(file)
-            root = tree.getroot()
-            for elem in root:
-                print("Elem:", elem.attrib)
-                sent_id = elem.get("id")
-                for subelem in elem:
-                    if subelem.tag == "entity":
-                        print("Subelem:", subelem.attrib)
-                        char_start_id, char_end_id = subelem.get("charOffset").split("-")
-                        self.data_df.loc[x] = [sent_id, 0, char_start_id, char_end_id, split]
-            print(self.data_df)
-            x+=1
-            if x >= 5:
-                break
-        return self.data_df, self.ner_df
+        self.fill_dfs(file_list)
        
+        
+        
+       # for file in file_list:
+       #     if str(file).split("/")[2] == "Test":
+       #         split = "test"
+       #     else:
+       #         split = "train"
+       #     print("file no", x)
+       #     print(file)
+       #     tree = ET.parse(file)
+       #     root = tree.getroot()
+       #     for elem in root:
+       #         print("Elem:", elem.attrib)
+       #         sent_id = elem.get("id")
+       #         for subelem in elem:
+       #             if subelem.tag == "entity":
+       #                 word = subelem.get("text").lower()
+       #                 if word in voc:  
+       #                     token_id = voc[word]
+       #                 else:
+       #                     voc[word] = len(voc)
+       #                     token_id = voc[word]
+       #                 print("Subelem:", subelem.attrib)
+       #                 if len(subelem.get("charOffset").split("-")) == 2:
+       #                     char_start_id, char_end_id = subelem.get("charOffset").split("-")
+       #                 else:
+       #                     char_start_id, char_end_id = (subelem.get("charOffset").split("-")[0], subelem.get("charOffset").split("-")[1].split(";")[1]), (subelem.get("charOffset").split("-")[1].split(";")[0], subelem.get("charOffset").split("-")[1])  
+                        # super ugly, used for discontinued 2-part words, tuberculosis (and something else) drugs, e.g., now represented as (40, 50), (45, 55) -> better to just take very first and very last?
+        #                ner_id = subelem.get("type") # chose to take all 4 categories, because less information loss, and big meaning differences...
+        #                self.data_df.loc[x] = [sent_id, token_id, char_start_id, char_end_id, split]
+        #                self.ner_df.loc[x] = [sent_id, ner_id, char_start_id, char_end_id]
+        #    print(voc)
+        #    x+=1
+            #if x >= 5:
+             #   break
+        return 
 
 
     def get_y(self):
